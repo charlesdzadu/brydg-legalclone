04e79db6b68e6e04d58a381129bda17a593bfe12
Djaglo Doris <dorisdjalo@gmail.com>
2023-10-19 14:05:11 +0000

    Update: Hero section init

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,13 @@
 import Nav from '@/components/nav'
+import Main from '@/components/main'
 export default function Home() {
   // console.log('Hello World')
   return (
     <>
-      <div className='bg-smooth max-w-screen-2xl px-5 mx-auto'>
+      <div className='w-full bg-smooth max-w-screen-2xl px-5 mx-auto'>
         <Nav />
+        <Main />
+        
       </div>
 
     </>
---
+++ components/line.tsx
@@ -0,0 +1,12 @@
+const Line = () => {
+  return (
+    <>
+      <div className="relative w-[1.5px] mx-1 lg:mx-6 flex flex-col items-center ">
+        <div className=" bg-my-secondary  w-5 h-5 rounded-full"></div>
+        <div className="h-screen w-full bg-my-secondary"></div>
+      </div>
+    </>
+  )
+}
+
+export default Line
